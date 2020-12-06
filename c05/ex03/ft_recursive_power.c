73365d5ff8a8d4b67e8b7f69e60dd440eef08db2
franzudev <fede.franzu@gmail.com>
2020-12-07 00:53:09 +0100

    edit ex03

--- c05/ex03/ft_recursive_power.c
+++ c05/ex03/ft_recursive_power.c
@@ -2,18 +2,13 @@
 
 int ft_recursive_power(int nb, int power)
 {
-	int neg;
-
-	neg = 1;
-	if (nb == 0 || power == 0)
+	if (nb < 0 || power < 0)
 		return 1;
-	if (nb < 0 && power % 3 == 0)
-		neg *= -1;
-	return nb * ft_recursive_power(nb, power - 1) * neg;
+	return nb * ft_recursive_power(nb, power - 1);
 }
 
 int main(void)
 {
-	printf("%d", ft_recursive_power(5, 3));
+	printf("%d", ft_recursive_power(2, 6));
 	return (0);
 }
