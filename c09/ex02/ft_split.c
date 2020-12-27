ec2a92669b2b314d164f2279ec1718985e920de0
franzudev <fede.franzu@gmail.com>
2020-12-27 14:03:19 +0100

    norminette ex02

--- c09/ex02/ft_split.c
+++ c09/ex02/ft_split.c
@@ -1,25 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: franzu <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/27 13:16:56 by franzu            #+#    #+#             */
+/*   Updated: 2020/12/27 13:21:50 by franzu           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 #include <stdio.h>
 
-char **ft_split(char *str, char *charset)
+int		word_counter(char *str, char *charset)
 {
-	char **res;
+	int w;
+	int i;
+
+	w = 1;
+	i = 0;
+	while (str[i])
+	{
+		if ((str[i] == *charset && str[i + 1] != *charset && str[i + 1] != '\0')
+			|| (str[i] != *charset && i == 0))
+			w++;
+		i++;
+	}
+	return (w);
+}
+
+void	alloc_string_space(char **res, char *str, char *charset)
+{
 	int w;
 	int i;
 	int len;
 
 	i = 0;
-	w = 1;
-	len = 0;
-	while (str[i])
-	{
-		if ((str[i] == *charset && str[i + 1] != *charset && str[i + 1] != '\0') || (str[i] != *charset && i == 0))
-			w++;
-		i++;
-	}
-	i = 0;
-	res = (char **)malloc(sizeof(char **) * w);
 	w = 0;
+	len = 0;
 	while (str[i])
 	{
 		if (str[i] != *charset)
@@ -31,9 +50,21 @@ char **ft_split(char *str, char *charset)
 		}
 		i++;
 	}
+}
+
+char	**ft_split(char *str, char *charset)
+{
+	char	**res;
+	int		w;
+	int		i;
+	int		a;
+
+	a = 0;
 	i = 0;
+	w = word_counter(str, charset);
+	res = (char **)malloc(sizeof(char **) * w);
+	alloc_string_space(res, str, charset);
 	w = 0;
-	int a = 0;
 	while (str[i])
 	{
 		if (str[i] != *charset)
@@ -46,13 +77,12 @@ char **ft_split(char *str, char *charset)
 		i++;
 	}
 	res[w] = "\0";
-
-	return res;
+	return (res);
 }
 
 int main(void)
 {
-    char *str = "||stiamn fafjdfk|jòa<.|.asdfjelfj|o|andando|al||mare||adfad";
+	char *str = "||stiamn fafjdfk|jòa<.|.asdfjelfj|o|andando|al||mare||adfad";
 	char **res = ft_split(str, "a");
 	int i = 0;
 	while (res[i][0] != '\0')
