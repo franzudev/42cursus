b64c3436a1df47fb1ea49e3a09a3ad5697e54029
franzudev <fede.franzu@gmail.com>
2020-12-27 13:16:13 +0100

    norminette ex00

--- c09/ex00/ft_putstr.c
+++ c09/ex00/ft_putstr.c
@@ -1,8 +1,20 @@
-void ft_putchar(char c);
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: franzu <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/27 13:08:52 by franzu            #+#    #+#             */
+/*   Updated: 2020/12/27 13:10:58 by franzu           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void ft_putstr(char *str)
+void	ft_putchar(char c);
+
+void	ft_putstr(char *str)
 {
-	while(*str)
+	while (*str)
 		ft_putchar(*str++);
 	ft_putchar('\n');
 }
