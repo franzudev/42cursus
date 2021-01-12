fe396bb9b0be7b968b8d8c9508f137bde66d8672
Federico Francois <ffrancoi@e3r5p5.42roma.it>
2021-01-12 20:43:27 +0100

    update itoa

--- libft-done/ft_itoa.c
+++ libft-done/ft_itoa.c
@@ -12,20 +12,20 @@
 
 #include "libft.h"
 
-static int	digit_count(int n)
+static int	digit_count(long n)
 {
 	int count;
 
-	count = 0;
+	count = 1;
 	if (n < 0)
 	{
 		n *= -1;
 		count++;
 	}
-	while (n != 0)
+	while (n >= 10)
 	{
 		n /= 10;
-		++count;
+		count++;
 	}
 	return (count);
 }
@@ -33,26 +33,26 @@ static int	digit_count(int n)
 char		*ft_itoa(int n)
 {
 	char	*res;
+	long	t;
 	int		d;
 
-	if (n == -2147483648)
-		return ("-2147483648");
-	if (n == 0)
-		return ("0");
-	d = digit_count(n);
+	t = (long)n;
+	d = digit_count(t);
 	res = (char *)malloc(sizeof(char) * d);
 	if (!res)
 		return (0);
-	if (n < 0)
+	if (t < 0)
 	{
 		res[0] = '-';
-		n *= -1;
+		t *= -1;
 	}
 	res[d--] = '\0';
-	while (n)
+	if (t == 0)
+		res[d] = '0';
+	while (t)
 	{
-		res[d--] = (n % 10) + '0';
-		n /= 10;
+		res[d--] = (t % 10) + '0';
+		t /= 10;
 	}
 	return (res);
 }
