43d57843c96b78643e97ab565726582e0b76e0a0
franzudev <fede.franzu@gmail.com>
2021-01-28 09:55:18 +0100

    update

--- printf/print_di.c
+++ printf/print_di.c
@@ -7,18 +7,20 @@ void	print_di(va_list args, t_flags f, int *printed)
 	int		arg_len;
 	int		sign = 0;
 	int		s_printed = 0;
+	int		arg_len2;
 
 	arg.digit = va_arg(args, int);
 	sign = arg.digit < 0;
 	arg.str = ft_itoa(sign ? (arg.digit *= -1) : arg.digit);
 	arg_len = (sign) ? ft_strlen(arg.str) + 1 : ft_strlen(arg.str);
 	if (f.precision > f.width) {
-		f.width = (sign) ? ++f.precision : f.precision;
+//		f.width = (sign) ? ++f.precision : f.precision;
+		(sign) ? ++f.precision : f.precision;
 		f.zero_pad = 1;
 	}
-	if (f.precision > f.width && f.width > arg_len && !f.zero_pad)
+	if (f.precision > f.width && f.precision > arg_len)
 		f.zero_pad = 1;
-	if (sign && f.zero_pad)
+	if (sign && f.precision > arg_len && f.precision > f.width)
 	{
 		s_printed = 1;
 		ft_putchar_fd('-', 1);
@@ -27,24 +29,31 @@ void	print_di(va_list args, t_flags f, int *printed)
 	if (f.left_justify)
 	{
 		if (!s_printed && sign)
-			ft_putchar_fd('-', 1);
-		ft_putstr_fd(arg.str, 1);
-	}
-	if (f.width)
-	{
-		while (f.width - f.precision - arg_len > 0 && arg_len < f.precision)
 		{
-			if (f.zero_pad && !f.left_justify)
-				ft_putchar_fd('0', 1);
-			else
-				ft_putchar_fd(' ', 1);
-			*printed += 1;
-			f.width--;
+			ft_putchar_fd('-', 1);
+			s_printed = 1;
 		}
+		if (arg_len < f.precision || f.left_justify)
+			ft_putstr_fd(arg.str, 1);
+	}
+	arg_len2 = (f.precision > arg_len) ? f.precision : arg_len;
+	while (f.width - arg_len2 > 0)
+	{
+		ft_putchar_fd(f.zero_pad ? '0' : ' ', 1);
+		*printed += 1;
+		f.width--;
+	}
+	if (!s_printed && sign && !f.left_justify && f.zero_pad && f.precision < 0)
+		ft_putchar_fd('-', 1);
+	while (f.precision - arg_len > 0)
+	{
+		ft_putchar_fd('0', 1);
+		*printed += 1;
+		f.precision--;
 	}
 	if (!(f.left_justify))
 	{
-		if (!s_printed && sign)
+		if (sign && !s_printed)
 			ft_putchar_fd('-', 1);
 		ft_putstr_fd(arg.str, 1);
 	}
--- printf/test.c
+++ printf/test.c
@@ -6,10 +6,10 @@ int main()
 	// unsigned int n = 4294967295;
 	// int m = -2147483648;
 	// 	char *str = "%-10.% f %-*.3s\n";
-	// ft_printf("%-010% f %4.3s\n%p\nciao\n%-10c\n%-20.3di\n%020.3ii\n%020ui\n%030.4xi\n%50p\n",
+//	 ft_printf("%-010% f %4.3s\n%p\nciao\n%-10c\n%-20.3di\n%020.3ii\n%020ui\n%030.4xi\n%50p\n",
 	// 			"prova", &n,'Z', 2147483647, m, n, n, &n);
 	// printf("hello, %-09s.\n", NULL);
 	   printf("%8.5i\n", 34);
-	ft_printf("%8.5i", 34);
+	ft_printf("%07i", -54);
 	return 0;
 }
