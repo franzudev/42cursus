3dd4a364351e9e20f5c2623f8b9b3955998444f2
franzudev <fede.franzu@gmail.com>
2021-02-09 21:31:34 +0100

    update

--- ft_server/srcs/start.sh
+++ ft_server/srcs/start.sh
@@ -3,14 +3,14 @@
 GREEN='\033[0;32m'
 NC='\033[0m'
 
-service php7.3-fpm start
-echo "[ ${GREEN}ok${NC} ] Starting PHP service${NC}"
 service mysql start
 mysql -uroot -e "CREATE DATABASE wordpress;"
-mysql -uroot -e "GRANT ALL PRIVILEGES ON wordpress.* TO "root"@"localhost";"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON wordpress.* TO \"root\"@\"localhost\" with GRANT OPTION;"
 mysql -uroot -e "FLUSH PRIVILEGES;"
 
+service php7.3-fpm start
+echo "[ ${GREEN}ok${NC} ] Starting PHP service${NC}"
+
 service nginx start
-echo "[ ${GREEN}ok${NC} ] Starting NGINX service${NC}"
 
 tail -f /dev/null
