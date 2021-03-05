f08f4c95e7447dac2d75945a8493a1bd04380cd8
franzudev <fede.franzu@gmail.com>
2021-03-05 09:29:10 +0100

    source link

--- libasm/main.c
+++ libasm/main.c
@@ -9,3 +9,9 @@ int main()
 	printf("\n%d\n", ft_atoi_base("   	-+--1242235", "01234567"));
 	return (0);
 }
+
+https://www.tutorialspoint.com/assembly_programming/assembly_system_calls.htm
+https://cs.lmu.edu/~ray/notes/nasmtutorial/
+https://cdn.intra.42.fr/pdf/pdf/13296/en.subject.pdf
+https://cdn.intra.42.fr/pdf/pdf/12961/en.subject.pdf
+https://cdn.intra.42.fr/pdf/pdf/16387/en.subject.pdf
