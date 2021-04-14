599c83788e7409aeabebcce0f3d07a45b0a007ed
franzudev <fede.franzu@gmail.com>
2021-04-14 17:34:38 +0200

    add files

--- push_swap/checker.c
+++ push_swap/checker.c
@@ -1,24 +1,24 @@
-#include "libft/libft.h"
 #include "push_swap.h"
 
 #include <limits.h>
 
-void free_pointer(char *str)
+void	free_pointer(char *str)
 {
 	str = NULL;
 	free(str);
 }
 
-int get_next_line(char **line)
+int	get_next_line(char **line)
 {
-	char tmp[ARG_MAX];
-	int ret;
-	int i;
+	char	tmp[ARG_MAX];
+	int		ret;
+	int		i;
 
 	if (!line)
 		return (-1);
 	i = 0;
-	while ((ret = read(0, &tmp[i], 1)) > -1)
+	ret = read(0, &tmp[i], 1);
+	while (ret > -1)
 	{
 		if (tmp[i] == '\n' || ret == 0)
 		{
@@ -31,22 +31,22 @@ int get_next_line(char **line)
 			return (0);
 		}
 		i++;
+		ret = read(0, &tmp[i], 1);
 	}
 	return (-1);
 }
 
-int main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
-	t_data deta;
+	t_data	data;
 
-	if (argc == 100)
-		return (int)argv;
-	char *line;
-	line = malloc(100);
-	while (get_next_line(&line) != 0)
+	if (argc == 1)
+		exit(0);
+	check_args(&data, argc, argv);
+	while (get_next_line(&data.operation) > 0)
 	{
-		check_instruction(line);
-
+		check_instruction(data.operation);
+		exec_instruction(&data);
 	}
 	return (0);
 }
--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -1,12 +1,16 @@
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
+#include "libft/libft.h"
+
 typedef struct s_data {
 	int		*stack_a;
 	int		size_a;
 	int		*stack_b;
 	int		size_b;
-	char	*operation;
+	char	operation[4];
 }			t_data;
+
+#define ERR "Error\n"
 
 #endif
