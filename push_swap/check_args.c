7e389588f290fb90c06b0d2c550daf576550819e
lince <lincerpi@student.42roma.it>
2021-04-16 14:40:58 +0200

    topolino

--- push_swap/check_args.c
+++ push_swap/check_args.c
@@ -30,57 +30,62 @@ static void	check_number(char *number, t_data *data)
 	}
 }
 
-static void	fill_stack_a(t_data *data, char **shit)
+static void	fill_stack_a(t_data *data, char **str, int i)
 {
-	int	i;
 	int	nb;
 
-	i = -1;
-	while (++i <= data->size_a)
+	if (i == -1)
 	{
-		check_number(shit[i], data);
-		nb = ft_atoi_2(shit[i], data);
-		data->stack_a[i - 1] = nb;
+		while (++i < data->size_a)
+		{
+			check_number(str[i], data);
+			nb = ft_atoi_2(str[i], data);
+			data->stack_a[i] = nb;
+		}
+	}
+	else
+	{
+		while (++i <= data->size_a)
+		{
+			check_number(str[i], data);
+			nb = ft_atoi_2(str[i], data);
+			data->stack_a[i - 1] = nb;
+		}
 	}
 	check_duplicate(data);
 }
 
-static char **check_string(t_data *data, int argc, char **argv)
+static char	**check_size(int argc, char **argv, t_data *data)
 {
-	char **shit;
-	int i;
+	char	**shit;
+	int		i;
 
 	if (argc == 2)
 	{
 		shit = ft_split(argv[1], ' ');
 		i = 0;
-		while(**shit)
+		while (shit[i])
 			i++;
 		data->size_a = i;
+		return (shit);
 	}
 	else
-	{
-		i = 1;
-		while (*argv[i])
-		{
-			*shit[i - 1] = *argv[i];
-			i++;
-		}
-			data->size_a = argc - 1;
-	}
-	return (shit);
+		data->size_a = argc - 1;
+	return (NULL);
 }
 
 void	check_args(t_data *data, int argc, char **argv)
 {
-	char **shit;
+	char	**shit;
 
-	shit = check_string(data, argc, argv);
-	//data->size_a = argc - 1;
+	shit = check_size(argc, argv, data);
 	data->size_b = 0;
 	data->stack_a = (int *)malloc(sizeof(int) * data->size_a);
 	data->stack_b = (int *)malloc(sizeof(int) * data->size_a);
 	if (!data->stack_a || !data->stack_b)
 		ft_exit(ERR, data);
-	fill_stack_a(data, shit);
+	if (argc == 2)
+		fill_stack_a(data, shit, -1);
+	else
+		fill_stack_a(data, argv, 0);
 }
