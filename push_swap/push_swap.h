e3f8f7b92a93190393892a91cddcf4312136a418
lince <lincerpi@student.42roma.it>
2021-04-18 20:25:09 +0200

    quasi fatto

--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -15,6 +15,13 @@ typedef struct s_data {
 	char	*operation;
 }			t_data;
 
+typedef struct s_sort {
+	int		*stack;
+	int		size;
+	int		index_mediana;
+	int		mediana;
+}			t_sort;
+
 int		ft_atoi_2(const char *str, t_data *data);
 void	print_result(t_data *data);
 void	free_pointer(void *pointer);
@@ -31,5 +38,8 @@ int		is_sorted_asc(const int *stack, int size);
 int		is_sorted_desc(const int *stack, int size);
 void	ft_quicksort(int *a, int first, int last);
 void	sort_array(t_data *data);
+
+void	pincopallino(t_data *data);
+void	topolino(t_data *data);
 
 #endif
