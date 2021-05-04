8906a323e03f559486bcf7872f5e605a401f18bd
franzudev <fede.franzu@gmail.com>
2021-05-04 13:55:05 +0200

    add debugger

---
+++ minishell/cmake-build-debug/CMakeFiles/minishell.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/franzu/42/minishell/main.c" "/Users/franzu/42/minishell/cmake-build-debug/CMakeFiles/minishell.dir/main.c.o"
+  "/Users/franzu/42/minishell/srcs/env.c" "/Users/franzu/42/minishell/cmake-build-debug/CMakeFiles/minishell.dir/srcs/env.c.o"
+  "/Users/franzu/42/minishell/srcs/utils.c" "/Users/franzu/42/minishell/cmake-build-debug/CMakeFiles/minishell.dir/srcs/utils.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
