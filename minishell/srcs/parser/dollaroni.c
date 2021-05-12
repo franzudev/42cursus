a3cc8564f0cf98bdc4a75f79334311f94e0f05c8
Luca Incerpi <lincerpi@e3r6p1.42roma.it>
2021-05-12 20:15:37 +0200

    dramma

---
+++ minishell/srcs/parser/dollaroni.c
@@ -0,0 +1,16 @@
+#include "../../minishell.h"
+
+
+// fai indexof e trova index
+// fai substr fino index
+// fai strjoin di temp e valore $
+// ripwti finche muori
+
+char	*ft_dollaroni(char *usd)
+{
+	int index;
+	char	*canada;
+
+	index = ft_index_of(usd, "$");
+	
+}
