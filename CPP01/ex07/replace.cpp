32e1ff52f32cc0250f4115addbac332ddda8fbdf
franzudev <fede.franzu@gmail.com>
2021-05-26 19:45:09 +0200

    ex07

---
+++ CPP01/ex07/replace.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Federico Francois on 26/05/2021.
+//
+
+#include "replace.h"
---
+++ CPP01/ex07/replace.h
@@ -0,0 +1,8 @@
+//
+// Created by Federico Francois on 26/05/2021.
+//
+
+#ifndef CPP01_REPLACE_H
+#define CPP01_REPLACE_H
+
+#endif //CPP01_REPLACE_H
