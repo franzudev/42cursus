f788c7b562132fb438305e305d1b2f1545a87bbb
Mattia Pezzullo <mpezzull@e3r12p1.42roma.it>
2022-02-26 22:02:07 +0100

    Added several css theme, create AppWrapper and App with a fixed menu. Added sass to dependency

--- ft_transcendence/frontend/src/main.ts
+++ ft_transcendence/frontend/src/main.ts
@@ -1,12 +1,26 @@
-import { createApp } from 'vue'
+import { createApp, reactive } from 'vue'
 import { createPinia } from 'pinia'
 
-import App from './App.vue'
-import router from './router'
+import './assets/styles/layout.scss';
 
-const app = createApp(App)
+import App from './App.vue';
+import AppWrapper from './AppWrapper.vue';
+import router from './router';
+import PrimeVue from 'primevue/config';
+import InputText from 'primevue/inputtext';
+import Menubar from 'primevue/menubar';
+
+const app = createApp(AppWrapper)
+app.config.globalProperties.$appState = reactive({ theme: 'lara-light-indigo', darkTheme: false });
 
 app.use(createPinia())
-app.use(router)
+app.use(router);
+app.use(PrimeVue);
+app.component('InputText', InputText);
+app.component('Menubar', Menubar);
+app.component('App', App);
+
+
 
 app.mount('#app')
+
