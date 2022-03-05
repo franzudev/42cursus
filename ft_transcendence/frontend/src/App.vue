6d17dc832d7e9086325f44ae67f560ac7aa2f71c
Mattia Pezzullo <mpezzull@e3r11p1.42roma.it>
2022-03-05 20:21:19 +0100

    removing themes, update tsconfig

--- ft_transcendence/frontend/src/App.vue
+++ ft_transcendence/frontend/src/App.vue
@@ -1,92 +1,94 @@
-<script lang="ts">
-import { RouterView } from "vue-router"
+<script setup lang="ts">
+import { defineComponent, reactive } from "vue";
+import { useAppStore } from "./stores/app";
 
-export default {
-  data() {
-    return {
-      nestedMenuitems: [
-        {
-          label: 'Profile',
-          icon: 'pi pi-fw pi-user'
-        },
-        {
-          label: 'Game',
-          icon: 'pi pi-fw pi-table',
-          items: [
-            {
-              label: 'New Game',
-              icon: 'pi pi-fw pi-user-plus',
-              items: [
-                {
-                  label: 'Play with a friend',
-                  icon: 'pi pi-fw pi-plus'
-                },
-                {
-                  label: 'Play with a random user',
-                  icon: 'pi pi-fw pi-copy'
-                },
+console.log("init")
 
-              ]
-            },
-            {
-              label: 'Watch a game',
-              icon: 'pi pi-fw pi-user-edit'
-            }
-          ]
-        },
-        {
-          label: 'History',
-          icon: 'pi pi-fw pi-shopping-cart',
-          items: [
-            {
-              label: 'View',
-              icon: 'pi pi-fw pi-list'
-            },
-            {
-              label: 'Search',
-              icon: 'pi pi-fw pi-search'
-            },
-
-          ]
-        },
-        {
-          label: 'Chat',
-          icon: 'pi pi-fw pi-envelope',
-          items: [
-            {
-              label: 'Send a message',
-              icon: 'pi pi-fw pi-compass'
-
-            },
-            {
-              label: 'Join a room',
-              icon: 'pi pi-fw pi-map-marker'
-
-            },
-            {
-              label: 'Create a Room',
-              icon: 'pi pi-fw pi-pencil'
-            }
-          ]
-        }
-      ]
-    }
+const menuBar = reactive([
+  {
+    label: 'Game',
+    icon: 'pi pi-fw pi-table',
+    items: [
+      {
+        label: 'New Game',
+        icon: 'pi pi-fw pi-user-plus',
+        items: [
+          {
+            label: 'Play with a friend',
+            icon: 'pi pi-fw pi-plus'
+          },
+          {
+            label: 'Play with a random user',
+            icon: 'pi pi-fw pi-copy'
+          },
+        ]
+      },
+      {
+        label: 'Watch a game',
+        icon: 'pi pi-fw pi-user-edit'
+      }
+    ]
+  },
+  {
+    label: 'History',
+    icon: 'pi pi-fw pi-shopping-cart',
+    items: [
+      {
+        label: 'View',
+        icon: 'pi pi-fw pi-list'
+      },
+      {
+        label: 'Search',
+        icon: 'pi pi-fw pi-search'
+      },
+    ]
+  },
+  {
+    label: 'Chat',
+    icon: 'pi pi-fw pi-envelope',
+    items: [
+      {
+        label: 'Send a message',
+        icon: 'pi pi-fw pi-compass'
+      },
+      {
+        label: 'Join a room',
+        icon: 'pi pi-fw pi-map-marker'
+      },
+      {
+        label: 'Create a Room',
+        icon: 'pi pi-fw pi-pencil'
+      }
+    ]
   }
+])
+
+const store = useAppStore()
+
+function changeTheme() {
+  const { isDark, themeColor } = store
+  const themeElement: HTMLElement = document.getElementById('theme-link') as HTMLElement;
+  themeElement.setAttribute('href', themeElement.getAttribute('href')!.replace(themeColor, isDark ? "light" : "dark" ));
+  store.setTheme()
 }
+
 </script>
+
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-    <Menubar :model="nestedMenuitems">
-      <template #end>
-        <span class="p-input-icon-left">
-          <i class="pi pi-search" />
-          <InputText type="text" placeholder="Search" />
-        </span>
+    <Menubar :model="menuBar">
+      <template #start>
+        <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="50" height="50" />
       </template>
     </Menubar>
   </header>
+  <button @click="changeTheme">prova</button>
 
   <RouterView />
 </template>
+
+
+<style lang="scss">
+@import "./assets/styles/layout.scss";
+</style>
--- ft_transcendence/frontend/src/main.ts
+++ ft_transcendence/frontend/src/main.ts
@@ -4,23 +4,19 @@ import { createPinia } from 'pinia'
 import './assets/styles/layout.scss';
 
 import App from './App.vue';
-import AppWrapper from './AppWrapper.vue';
 import router from './router';
 import PrimeVue from 'primevue/config';
 import InputText from 'primevue/inputtext';
 import Menubar from 'primevue/menubar';
 
-const app = createApp(AppWrapper)
-app.config.globalProperties.$appState = reactive({ theme: 'lara-light-indigo', darkTheme: false });
+const app = createApp(App)
+app.config.globalProperties.$appState = reactive({ theme: 'lara-dark-indigo', darkTheme: true });
 
+app.use(PrimeVue, { ripple: true, inputStyle: 'outlined' });
 app.use(createPinia())
 app.use(router);
-app.use(PrimeVue);
 app.component('InputText', InputText);
 app.component('Menubar', Menubar);
-app.component('App', App);
-
-
 
 app.mount('#app')
 
