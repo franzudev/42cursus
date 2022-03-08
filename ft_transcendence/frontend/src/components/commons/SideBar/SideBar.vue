8d840b6ad4b06fcc348e98e8f0290a5ae0917756
Mattia Pezzullo <mpezzull@e3r11p1.42roma.it>
2022-03-08 06:01:57 +0100

    rooms selectable and event emitted

--- ft_transcendence/frontend/src/components/commons/SideBar/SideBar.vue
+++ ft_transcendence/frontend/src/components/commons/SideBar/SideBar.vue
@@ -1,18 +1,39 @@
 <script setup lang="ts">
-import { defineComponent, reactive } from "vue";
+import { computed, defineComponent, reactive } from "vue";
 import { useAppStore } from "@/stores/app";
-import ChatRoomItem from "../ChatRoomItem/ChatRoomItem.vue";
 import { ref, onMounted } from 'vue';
 import ChatRoomService from "@/service/ChatRoomService";
+
+const emit = defineEmits<{
+    (e: 'selectedRoom', id: number): void
+}>()
 
 onMounted(() => {
     chatRoomService.value.getRooms().then(data => rooms.value = data);
     console.log(rooms.value);
 })
 
-const rooms = ref([{ "id": Number, "name": String, "description": String, "image": String }]);
-const chatRoomService = ref(new ChatRoomService());
+function onItemClick(event: MouseEvent, id: number) {
+    emit("selectedRoom", id);
+    console.log("emit" + id);
+    SelectRoom(id);
+}
 
+function SelectRoom(id: number)
+{
+    selected.value.id = id;
+}
+
+function    getClass(id: number)
+{
+    if (id == selected.value.id)
+        return "chatroom-item p-highlight";
+    return "chatroom-item";
+
+}
+const selected = ref({id: Number()});
+const rooms = ref([{ "id": Number(), "name": String(), "description": String(), "image": String() }]); //non funziona senza le parentesi?
+const chatRoomService = ref(new ChatRoomService());
 </script>
 
 
@@ -21,7 +42,7 @@ const chatRoomService = ref(new ChatRoomService());
         <div class="p-chatroom-title">Chat Room List</div>
         <div class="p-chatrooms" v-for="room in rooms">
             <!-- <ChatRoomItem /> -->
-            <div class="chatroom-item">
+            <li :class="getClass(room.id)" @click="onItemClick($event, room.id)">
                 <div class="image-container">
                     <img
                         :src="room.image"
@@ -29,9 +50,9 @@ const chatRoomService = ref(new ChatRoomService());
                         style="width:30px; height:30px; border-radius: 30px;"
                     />
                 </div>
-                <div class="chatroom-list-detail ">{{ room.name }} </div>
+                <div class="chatroom-list-detail">{{ room.name }}</div>
                 <!-- <div>{{ room.description }}</div> -->
-            </div>
+            </li>
         </div>
     </div>
 </template>
@@ -44,23 +65,23 @@ const chatRoomService = ref(new ChatRoomService());
 }
 
 .chatroom-item {
-	display: flex;
-	align-items: center;
-	padding: .5rem;
-	width: 100%;
+    display: flex;
+    align-items: center;
+    padding: 0.5rem;
+    width: 100%;
 
-	img {
-		width: 75px;
+    img {
+        width: 75px;
         box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
         margin-right: 1rem;
-	}
+    }
 
-	.chatroom-list-detail {
-		flex: 1 1 0;
-	}
+    .chatroom-list-detail {
+        flex: 1 1 0;
+    }
 
-	.chatroom-list-action {
-		display: flex;
+    .chatroom-list-action {
+        display: flex;
         flex-direction: column;
         align-items: flex-end;
     }
