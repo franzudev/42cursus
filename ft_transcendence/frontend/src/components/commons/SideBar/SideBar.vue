131b024cf4bb7cb201e0deccc2c42dbad343f440
Mattia Pezzullo <mpezzull@e3r11p1.42roma.it>
2022-03-08 03:58:15 +0100

    wip list of chat rooms

--- ft_transcendence/frontend/src/components/commons/SideBar/SideBar.vue
+++ ft_transcendence/frontend/src/components/commons/SideBar/SideBar.vue
@@ -10,7 +10,7 @@ onMounted(() => {
     console.log(rooms.value);
 })
 
-const rooms = ref(null);
+const rooms = ref([{ "id": Number, "name": String, "description": String, "image": String }]);
 const chatRoomService = ref(new ChatRoomService());
 
 </script>
@@ -18,17 +18,67 @@ const chatRoomService = ref(new ChatRoomService());
 
 <template>
     <div class="sidebar">
-        <li v-for="room in rooms">
+        <div class="p-chatroom-title">Chat Room List</div>
+        <div class="p-chatrooms" v-for="room in rooms">
             <!-- <ChatRoomItem /> -->
-         {{   room.name }}
-        </li>
+            <div class="chatroom-item">
+                <div class="image-container">
+                    <img
+                        :src="room.image"
+                        :alt="room.name"
+                        style="width:30px; height:30px; border-radius: 30px;"
+                    />
+                </div>
+                <div class="chatroom-list-detail ">{{ room.name }} </div>
+                <!-- <div>{{ room.description }}</div> -->
+            </div>
+        </div>
     </div>
 </template>
 
 <style lang="scss">
 .sidebar {
-    width: 200px;
+    width: 250px;
     height: calc(100vh - 69px);
-    background-color: black;
+    // background-color: black; moved to theme.css
+}
+
+.chatroom-item {
+	display: flex;
+	align-items: center;
+	padding: .5rem;
+	width: 100%;
+
+	img {
+		width: 75px;
+        box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+        margin-right: 1rem;
+	}
+
+	.chatroom-list-detail {
+		flex: 1 1 0;
+	}
+
+	.chatroom-list-action {
+		display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+    }
+}
+
+@media screen and (max-width: 576px) {
+    .chatroom-item {
+        flex-wrap: wrap;
+
+        .image-container {
+            width: 100%;
+            text-align: center;
+        }
+
+        img {
+            margin: 0 0 1rem 0;
+            width: 100px;
+        }
+    }
 }
 </style>
--- ft_transcendence/frontend/src/service/ChatRoomService.ts
+++ ft_transcendence/frontend/src/service/ChatRoomService.ts
@@ -1,6 +1,6 @@
 export default class ChatRoomService {
 
-	async getRooms() {
+	getRooms() {
 		return fetch('/data/rooms.json').then(res => res.json()).then(d => d.data);
     }
 }
