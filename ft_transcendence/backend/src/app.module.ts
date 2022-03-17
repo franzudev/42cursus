f46097aa34e93052e971c9685fbc318e3e668d43
franzudev <fede.franzu@gmail.com>
2022-03-17 15:26:49 +0100

    update: database config from env

--- ft_transcendence/backend/src/app.module.ts
+++ ft_transcendence/backend/src/app.module.ts
@@ -1,4 +1,3 @@
-import { User } from './users/entities/user.entity';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
@@ -8,20 +7,17 @@ import { RoomsModule } from './rooms/rooms.module';
 import { AuthModule } from './auth/auth.module';
 import { PassportModule } from '@nestjs/passport'
 import { ConfigModule } from "@nestjs/config";
+import { DatabaseConfigService } from "./database-config.service";
 
 @Module({
   imports: [
     PassportModule.register({ session: true }),
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'db',
-      port: 5432,
-      username: 'postgres',
-      password: 'password',
-      database: 'transcendence',
-      entities: [User],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: DatabaseConfigService,
+      inject: [DatabaseConfigService]
     }),
     AuthModule,
     UsersModule,
