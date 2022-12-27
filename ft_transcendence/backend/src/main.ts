b1e4eefe480ae1ebbbc550275fbd9aad845209cf
franzudev <28902523+franzudev@users.noreply.github.com>
2022-12-27 10:38:04 +0100

    refactor(auth): clean up and replace hard coded values

--- ft_transcendence/backend/src/main.ts
+++ ft_transcendence/backend/src/main.ts
@@ -5,32 +5,34 @@ import * as session from 'express-session';
 import * as passport from 'passport'
 // somewhere in your initialization file
 import * as cookieParser from 'cookie-parser';
+import {ConfigService} from "@nestjs/config"
 
 // somewhere in your initialization file
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.use(
-    session({
-      cookie: {
-        maxAge: 60000 * 60 * 24 // 1 m per 60m * 24 h
-      },
-      secret: 'my-secret', // cookie encryption
-      resave: false,
-      saveUninitialized: false, // save sessions only if user is logged in
-    }),
-  );
-  app.use(passport.initialize());
-  app.use(passport.session());
-  app.use(cookieParser());
-  app.enableCors({
-      origin: "*"
-  })
+    const app = await NestFactory.create(AppModule);
+    const configService = app.get(ConfigService);
 
-  app.enableCors({
-    credentials: true, origin: 'http://localhost:3000'
-  });
+    app.use(
+        session({
+            cookie: {
+                maxAge: parseInt(configService.get("COOKIE_MAX_AGE"))
+            },
+            secret: configService.get("COOKIE_SECRET"),
+            resave: false,
+            saveUninitialized: false, // save sessions only if user is logged in
+        }),
+    );
+    app.use(passport.initialize());
+    app.use(passport.session());
+    app.use(cookieParser());
 
-  await app.listen(5050);
+    app.enableCors({
+        credentials: true,
+        origin: `${configService.get("APP_URL")}`
+    });
+
+    await app.listen(5050);
 }
+
 bootstrap();
